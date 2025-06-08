12212b0efc598a629eefa03db76b871eac57f646
maaz akhlaq <maaz.sardar355@gmail.com>
2025-06-08 21:10:45 +0300

    1st commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+
+import SkipSelection from './components/SkipSelection/SkipSelection'
+const App = () => <SkipSelection />
+ 
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+     
+      
+      colors: {
+     
+        borderInput: "#36A2EB",      
+       
+       
+     
+      },
+      
+    },
+  },
+  plugins: [],
+};
